eece61831b48b7e4a46b7113d03e955fa21154c1
Ramy Abdel-Azim <ramyabdelazim@gmail.com>
2012-02-14 02:23:38 -0500

    hello world

--- config/routes.rb
+++ config/routes.rb
@@ -56,4 +56,6 @@ LooneyBin::Application.routes.draw do
   # Note: This route will make all actions in every controller accessible via GET requests.
   # match ':controller(/:action(/:id(.:format)))'
 
+  match "/" => "talent#index"
+
 end
